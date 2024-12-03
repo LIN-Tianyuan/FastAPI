d3fe2a679d31e55b858a9d38e98cf0f69fd4f6b2
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2024-12-03 01:10:19 +0100

    Create User

--- blog/schemas.py
+++ blog/schemas.py
@@ -7,5 +7,11 @@ class Blog(BaseModel):
 class ShowBlog(BaseModel):
     title: str
     body: str
+    
     class Config():
         orm_mode = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
